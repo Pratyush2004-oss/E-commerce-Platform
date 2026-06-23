2cde2b8dd28edb510c79e6767b22f69f688930a6
Pratyush2004-oss <pratyush@sitp.ac.in>
2026-06-23 15:39:25 +0530

    Adding changes

--- backend/controllers/item.controller.js
+++ backend/controllers/item.controller.js
@@ -1,1 +1,7 @@
- export const addItem = (req, res) => {};
+ export const addItem = (req, res) => {
+    const {} = req.body
+
+    res.status(200).json({
+        message: "Item added successfully"
+    })
+ };
